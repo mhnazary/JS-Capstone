76ed1c30ad7eb6300b9c07b6770171bcb7cedcf7
Mahram Hossain <mhnazary2020@yahoo.com>
2023-06-06 20:25:35 +0430

    New changes added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import Icon from './assets/Icons/logo.png';
+
+// Find the logo image element and set its src attribute
+const logoImage = document.querySelector('.logo');
+logoImage.src = Icon;
