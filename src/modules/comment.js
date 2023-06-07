d5fded25d7ddbed4a319c8644f6173b348a81a58
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-08 01:12:36 +0300

    Add the class for geting and adding comments from the api

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,47 +1,39 @@
 class Comment {
-    constructor() {
-      this.id = 'LnXwgVRrp2EsjCl5tvcX';
-      this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+  constructor() {
+    this.id = 'EK8AqlUP7MtIYG7gJYqn';
+    this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+  }
+
+  async getcomment(itemid) {
+    try {
+      const response = await fetch(`${this.url}apps/${this.id}/comments?item_id=${itemid}`);
+      const comments = await response.json();
+      return comments;
+    } catch (error) {
+      return [];
     }
-    async getcomment(itemid) {
-        try {
-          const response = await fetch(`${this.url}apps/${this.id}/comments?item_id=${itemid}`);
-          const comments = await response.json();
-          console.log('getcomment response:', comments);
-          return comments.result;
-          
-        } catch (error) {
-          console.error('Error in getcomment method:', error);
-          return [];
-        }
-      }
-      
-      
-      
-  
-      async addcomments(username, comment, item_id) {
-  try {
-    const requestBody = {
-        item_id: item_id,
-        username: username,
-        comment: comment
+  }
+
+  async addcomments(username, comment, id) {
+    try {
+      const requestBody = {
+        item_id: id,
+        username,
+        comment,
       };
-    const response = await fetch(`${this.url}apps/${this.id}/comments`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestBody),
-    });
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    return null;
+      const response = await fetch(`${this.url}apps/${this.id}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return null;
+    }
   }
 }
 
-      
-  }
-  
-  export default Comment;
-  
+export default Comment;
