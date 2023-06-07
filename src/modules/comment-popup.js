ba8a60ff3a90aa46b9343d0e077722ff6dade17e
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-07 19:18:28 +0300

    Add the eventlistner for adding the comment

--- src/modules/comment-popup.js
+++ src/modules/comment-popup.js
@@ -1,6 +1,6 @@
 import closeIcon from '../assets/Icons/close.png';
 
-const modal = async (item) => {
+const modal = async (item ,commetObj,lists) => {
   const popup = document.querySelector('#popup-container');
   popup.innerHTML = `
     <div class="modal">
@@ -39,5 +39,31 @@ const modal = async (item) => {
   document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('#popup-container').style.display = 'none';
   });
+  document.querySelector('#add-btn').addEventListener('click', (event) => {
+    add(event, commetObj, lists);
+  });
+
+  const add = async (event, commetObj, lists) => {
+    const nameInputElement = document.getElementById('name-input');
+    const commentInputElement = document.getElementById('comment-input');
+    event.preventDefault();
+  
+    // Get the values from the input fields
+    const name = nameInputElement.value;
+    const comments = commentInputElement.value;
+    const date = new Date().toISOString().slice(0, 10);
+    // Add the comments to the item
+    await commetObj.addcomments(name, comments,item.id);
+  
+    // Refresh the comments list for the item
+    const commentsarr = await commetObj.getcomment(item.id);
+    console.log(commentsarr)
+    await lists(commentsarr, date);
+  
+    // Clear the input fields
+    nameInputElement.value = '';
+    commentInputElement.value = '';
+  };
+  
 };
 export default modal;
