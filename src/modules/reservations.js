241c422f133abeca8ddd6aa2fe13492b9c0a9000
JuanDBta <juandavid_diaz@hotmail.com>
2023-06-06 19:17:07 -0500

    Add reservations file for generate pop up window

--- src/modules/reservations.js
+++ src/modules/reservations.js
@@ -1,14 +1,44 @@
 import closeX from '../assets/Icons/close-circle-sharp.svg';
-import tvShow from '../assets/TvShows/Succesion.jpg';
 
-const tvShowLocation = document.querySelector('.tvshow_img');
-const tvShowImg = document.createElement('img');
-tvShowImg.src = tvShow;
-tvShowImg.alt = '';
-tvShowLocation.appendChild(tvShowImg);
+const modalReserve = async (item) => {
+  const popupReserve = document.querySelector('#reservation_page');
+  popupReserve.innerHTML = `
+    <div id="subcontainer">
+      <div id="head_reservations">
+        <div class="tvshow_img"><img src="${item.image}" alt=""></div>
+        <div class="closeX_box"><a class="closeX" href="#"><img src="${closeX}" alt =""></a></div>
+      </div>
+    
+      <div id="show_description">
+        <h3>${item.name}</h3>
+        <div id="description_card">
+          <span class="feature"><b><i>Language :</i></b>${item.language} </span>
+          <span class="feature"><b><i>Run time  :</i></b>${item.runtime}</span>
+          <span class="feature"><b><i>Geners  :</i></b>${item.genres.join(', ')}</span>
+          <span class="feature"><b><i>Language :</i></b>${item.language} </span>
+        </div>
+      </div>
 
-const closeLocation = document.querySelector('.closeX');
-const closeImg = document.createElement('img');
-closeImg.src = closeX;
-closeImg.alt = '';
-closeLocation.appendChild(closeImg);
+      <div id="reservations">
+        <h4 id="reservs_title">Reservations<span>0</span></h4>
+        <ul id="reservations_list">
+        </ul>
+      </div>
+
+      <form id="add_reservations">
+        <h4 id="reservs_form">Add a reservation</h4>
+        <input type="text" name="name" id="add-name" placeholder="Your name" required>
+        <input type="date" name="start" id="start_date" placeholder="Start date" required>
+        <input type="date" name="end" id="end_date" placeholder="End date" required>
+        <button type="submit" id="reserve_button">Reserve</button>
+      </form>
+
+    </div>
+    `;
+  popupReserve.style.display = 'flex';
+  document.querySelector('.closeX').addEventListener('click', () => {
+    document.querySelector('#reservation_page').style.display = 'none';
+  });
+};
+export default modalReserve;
+
