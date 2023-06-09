8683590af374f59706eb597f9b8310c9288f4742
Mahram Hossain <mhnazary2020@yahoo.com>
2023-06-09 09:02:43 +0430

    Expand cardCounter.js

---
+++ src/modules/counter.js
@@ -0,0 +1,7 @@
+const $ = document
+
+const cardCounter = (counter) => {
+    const filmcounter = $.getElementById('count');
+    filmcounter.textContent = counter;
+    };
+export default cardCounter;
